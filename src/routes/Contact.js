e1e875fd5fbf41bc21eaf34bf383dbfbfccb244c
nHs2415 <nuwanihansika92@gmail.com>
2024-05-21 17:04:57 -0700

    contact form done[Traveller]

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -2,6 +2,7 @@ import NavBar from "../NavBar";
 import Hero from "../components/Hero";
 import ContactImg from '../images/image10.jpg'
 import Footer from '../components/Footer';
+import ContactForm from "../components/ContactForm";
 
 function Contact () {
     return(
@@ -13,6 +14,7 @@ function Contact () {
             heroImg={ContactImg}
             title="Contact"
             />
+            <ContactForm/>
             <Footer/>
         
         </>
